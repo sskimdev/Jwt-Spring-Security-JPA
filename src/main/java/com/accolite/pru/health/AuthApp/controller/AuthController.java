835f1f47b7f4b5a9749eb838a40fbb4789f5a566
sskim <sskim@MacBook-Pro-2.local>
2020-08-08 18:59:26 +0900

    update

--- src/main/java/com/accolite/pru/health/AuthApp/controller/AuthController.java
+++ src/main/java/com/accolite/pru/health/AuthApp/controller/AuthController.java
@@ -74,26 +74,26 @@ public class AuthController {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    /**
-     * Checks is a given email is in use or not.
-     */
-    @ApiOperation(value = "Checks if the given email is in use")
-    @GetMapping("/checkEmailInUse")
-    public ResponseEntity checkEmailInUse(@ApiParam(value = "Email id to check against") @RequestParam("email") String email) {
-        Boolean emailExists = authService.emailAlreadyExists(email);
-        return ResponseEntity.ok(new ApiResponse(true, emailExists.toString()));
-    }
-
-    /**
-     * Checks is a given username is in use or not.
-     */
-    @ApiOperation(value = "Checks if the given username is in use")
-    @GetMapping("/checkUsernameInUse")
-    public ResponseEntity checkUsernameInUse(@ApiParam(value = "Username to check against") @RequestParam(
-            "username") String username) {
-        Boolean usernameExists = authService.usernameAlreadyExists(username);
-        return ResponseEntity.ok(new ApiResponse(true, usernameExists.toString()));
-    }
+//    /**
+//     * Checks is a given email is in use or not.
+//     */
+//    @ApiOperation(value = "Checks if the given email is in use")
+//    @GetMapping("/checkEmailInUse")
+//    public ResponseEntity checkEmailInUse(@ApiParam(value = "Email id to check against") @RequestParam("email") String email) {
+//        Boolean emailExists = authService.emailAlreadyExists(email);
+//        return ResponseEntity.ok(new ApiResponse(true, emailExists.toString()));
+//    }
+//
+//    /**
+//     * Checks is a given username is in use or not.
+//     */
+//    @ApiOperation(value = "Checks if the given username is in use")
+//    @GetMapping("/checkUsernameInUse")
+//    public ResponseEntity checkUsernameInUse(@ApiParam(value = "Username to check against") @RequestParam(
+//            "username") String username) {
+//        Boolean usernameExists = authService.usernameAlreadyExists(username);
+//        return ResponseEntity.ok(new ApiResponse(true, usernameExists.toString()));
+//    }
 
 
     /**
@@ -138,46 +138,46 @@ public class AuthController {
                 .orElseThrow(() -> new UserRegistrationException(registrationRequest.getEmail(), "Missing user object in database"));
     }
 
-    /**
-     * Receives the reset link request and publishes an event to send email id containing
-     * the reset link if the request is valid. In future the deeplink should open within
-     * the app itself.
-     */
-    @PostMapping("/password/resetlink")
-    @ApiOperation(value = "Receive the reset link request and publish event to send mail containing the password " +
-            "reset link")
-    public ResponseEntity resetLink(@ApiParam(value = "The PasswordResetLinkRequest payload") @Valid @RequestBody PasswordResetLinkRequest passwordResetLinkRequest) {
-
-        return authService.generatePasswordResetToken(passwordResetLinkRequest)
-                .map(passwordResetToken -> {
-                    UriComponentsBuilder urlBuilder = ServletUriComponentsBuilder.fromCurrentContextPath().path("/password/reset");
-                    OnGenerateResetLinkEvent generateResetLinkMailEvent = new OnGenerateResetLinkEvent(passwordResetToken,
-                            urlBuilder);
-                    applicationEventPublisher.publishEvent(generateResetLinkMailEvent);
-                    return ResponseEntity.ok(new ApiResponse(true, "Password reset link sent successfully"));
-                })
-                .orElseThrow(() -> new PasswordResetLinkException(passwordResetLinkRequest.getEmail(), "Couldn't create a valid token"));
-    }
-
-    /**
-     * Receives a new passwordResetRequest and sends the acknowledgement after
-     * changing the password to the user's mail through the event.
-     */
-
-    @PostMapping("/password/reset")
-    @ApiOperation(value = "Reset the password after verification and publish an event to send the acknowledgement " +
-            "email")
-    public ResponseEntity resetPassword(@ApiParam(value = "The PasswordResetRequest payload") @Valid @RequestBody PasswordResetRequest passwordResetRequest) {
-
-        return authService.resetPassword(passwordResetRequest)
-                .map(changedUser -> {
-                    OnUserAccountChangeEvent onPasswordChangeEvent = new OnUserAccountChangeEvent(changedUser, "Reset Password",
-                            "Changed Successfully");
-                    applicationEventPublisher.publishEvent(onPasswordChangeEvent);
-                    return ResponseEntity.ok(new ApiResponse(true, "Password changed successfully"));
-                })
-                .orElseThrow(() -> new PasswordResetException(passwordResetRequest.getToken(), "Error in resetting password"));
-    }
+//    /**
+//     * Receives the reset link request and publishes an event to send email id containing
+//     * the reset link if the request is valid. In future the deeplink should open within
+//     * the app itself.
+//     */
+//    @PostMapping("/password/resetlink")
+//    @ApiOperation(value = "Receive the reset link request and publish event to send mail containing the password " +
+//            "reset link")
+//    public ResponseEntity resetLink(@ApiParam(value = "The PasswordResetLinkRequest payload") @Valid @RequestBody PasswordResetLinkRequest passwordResetLinkRequest) {
+//
+//        return authService.generatePasswordResetToken(passwordResetLinkRequest)
+//                .map(passwordResetToken -> {
+//                    UriComponentsBuilder urlBuilder = ServletUriComponentsBuilder.fromCurrentContextPath().path("/password/reset");
+//                    OnGenerateResetLinkEvent generateResetLinkMailEvent = new OnGenerateResetLinkEvent(passwordResetToken,
+//                            urlBuilder);
+//                    applicationEventPublisher.publishEvent(generateResetLinkMailEvent);
+//                    return ResponseEntity.ok(new ApiResponse(true, "Password reset link sent successfully"));
+//                })
+//                .orElseThrow(() -> new PasswordResetLinkException(passwordResetLinkRequest.getEmail(), "Couldn't create a valid token"));
+//    }
+//
+//    /**
+//     * Receives a new passwordResetRequest and sends the acknowledgement after
+//     * changing the password to the user's mail through the event.
+//     */
+//
+//    @PostMapping("/password/reset")
+//    @ApiOperation(value = "Reset the password after verification and publish an event to send the acknowledgement " +
+//            "email")
+//    public ResponseEntity resetPassword(@ApiParam(value = "The PasswordResetRequest payload") @Valid @RequestBody PasswordResetRequest passwordResetRequest) {
+//
+//        return authService.resetPassword(passwordResetRequest)
+//                .map(changedUser -> {
+//                    OnUserAccountChangeEvent onPasswordChangeEvent = new OnUserAccountChangeEvent(changedUser, "Reset Password",
+//                            "Changed Successfully");
+//                    applicationEventPublisher.publishEvent(onPasswordChangeEvent);
+//                    return ResponseEntity.ok(new ApiResponse(true, "Password changed successfully"));
+//                })
+//                .orElseThrow(() -> new PasswordResetException(passwordResetRequest.getToken(), "Error in resetting password"));
+//    }
 
     /**
      * Confirm the email verification token generated for the user during
@@ -192,31 +192,31 @@ public class AuthController {
                 .orElseThrow(() -> new InvalidTokenRequestException("Email Verification Token", token, "Failed to confirm. Please generate a new email verification request"));
     }
 
-    /**
-     * Resend the email registration mail with an updated token expiry. Safe to
-     * assume that the user would always click on the last re-verification email and
-     * any attempts at generating new token from past (possibly archived/deleted)
-     * tokens should fail and report an exception.
-     */
-    @GetMapping("/resendRegistrationToken")
-    @ApiOperation(value = "Resend the email registration with an updated token expiry. Safe to " +
-            "assume that the user would always click on the last re-verification email and " +
-            "any attempts at generating new token from past (possibly archived/deleted)" +
-            "tokens should fail and report an exception. ")
-    public ResponseEntity resendRegistrationToken(@ApiParam(value = "the initial token that was sent to the user email after registration") @RequestParam("token") String existingToken) {
-
-        EmailVerificationToken newEmailToken = authService.recreateRegistrationToken(existingToken)
-                .orElseThrow(() -> new InvalidTokenRequestException("Email Verification Token", existingToken, "User is already registered. No need to re-generate token"));
-
-        return Optional.ofNullable(newEmailToken.getUser())
-                .map(registeredUser -> {
-                    UriComponentsBuilder urlBuilder = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/auth/registrationConfirmation");
-                    OnRegenerateEmailVerificationEvent regenerateEmailVerificationEvent = new OnRegenerateEmailVerificationEvent(registeredUser, urlBuilder, newEmailToken);
-                    applicationEventPublisher.publishEvent(regenerateEmailVerificationEvent);
-                    return ResponseEntity.ok(new ApiResponse(true, "Email verification resent successfully"));
-                })
-                .orElseThrow(() -> new InvalidTokenRequestException("Email Verification Token", existingToken, "No user associated with this request. Re-verification denied"));
-    }
+//    /**
+//     * Resend the email registration mail with an updated token expiry. Safe to
+//     * assume that the user would always click on the last re-verification email and
+//     * any attempts at generating new token from past (possibly archived/deleted)
+//     * tokens should fail and report an exception.
+//     */
+//    @GetMapping("/resendRegistrationToken")
+//    @ApiOperation(value = "Resend the email registration with an updated token expiry. Safe to " +
+//            "assume that the user would always click on the last re-verification email and " +
+//            "any attempts at generating new token from past (possibly archived/deleted)" +
+//            "tokens should fail and report an exception. ")
+//    public ResponseEntity resendRegistrationToken(@ApiParam(value = "the initial token that was sent to the user email after registration") @RequestParam("token") String existingToken) {
+//
+//        EmailVerificationToken newEmailToken = authService.recreateRegistrationToken(existingToken)
+//                .orElseThrow(() -> new InvalidTokenRequestException("Email Verification Token", existingToken, "User is already registered. No need to re-generate token"));
+//
+//        return Optional.ofNullable(newEmailToken.getUser())
+//                .map(registeredUser -> {
+//                    UriComponentsBuilder urlBuilder = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/auth/registrationConfirmation");
+//                    OnRegenerateEmailVerificationEvent regenerateEmailVerificationEvent = new OnRegenerateEmailVerificationEvent(registeredUser, urlBuilder, newEmailToken);
+//                    applicationEventPublisher.publishEvent(regenerateEmailVerificationEvent);
+//                    return ResponseEntity.ok(new ApiResponse(true, "Email verification resent successfully"));
+//                })
+//                .orElseThrow(() -> new InvalidTokenRequestException("Email Verification Token", existingToken, "No user associated with this request. Re-verification denied"));
+//    }
 
     /**
      * Refresh the expired jwt token using a refresh token for the specific device
